3a610ca2f86d1606439a92ca683b17742b9b5d05
HenriqueChepli <henriquechepli@gmail.com>
2024-09-23 15:46:31 -0300

    FEAT: Teste #8

--- src/App.vue
+++ src/App.vue
@@ -5,21 +5,22 @@ import HeaderComponente from './components/HeaderComponente.vue';
 </script>
 
 <template>
-<HeaderComponente/>
+  <HeaderComponente />
   <router-view></router-view>
 </template>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 
-*{  font-family: "Poppins", sans-serif;
-    padding: 0;
-    border: 0;
-    margin: 0;
-    box-sizing: border-box;
-}
-a{
-  text-decoration: none;
+* {
+  font-family: "Poppins", sans-serif;
+  padding: 0;
+  border: 0;
+  margin: 0;
+  box-sizing: border-box;
 }
 
+a {
+  text-decoration: none;
+}
 </style>
