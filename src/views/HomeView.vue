ba955898e82870bf14c9ef9ab66ef5abf0c299d7
HenriqueChepli <henriquechepli@gmail.com>
2024-09-25 16:38:09 -0300

    FEAT: Desfazendo burrice de Henrique Chepli #12

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,35 +7,9 @@
   </div>
 </template>
 
-<script>
+<script setup>
 import MarcasComponente from '@/components/MarcasComponente.vue';
 
-export default {
-  components: {
-    MarcasComponente
-  },
-  data(){
-    return{
-      camisas: null,
-      calcas: null,
-      agasalhos: null,
-      camisa: null,
-      calca: null,
-      agasalho: null,
-      status: "Solicitado",
-    }
-  },
-  methods: {
-    async getCatalogo(){
-      const req = await fetch("http://localhost:3000/catalogo");
-      const data = await req.json();
-
-    }
-  },
-  mounted() {
-    this.getCatalogo()
-  }
-}
 </script>
 
 <style>
