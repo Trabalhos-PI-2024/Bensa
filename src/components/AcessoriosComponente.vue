358bb3643d9e4a98ac3d89b46b1511a8ad051747
HenriqueChepli <henriquechepli@gmail.com>
2024-10-01 15:16:17 -0300

    FEAT: Arrumando a Home Page #18

--- src/components/AcessoriosComponente.vue
+++ src/components/AcessoriosComponente.vue
@@ -1,83 +1,155 @@
 <template>
-    <div class="acessorios">
-        <div class="mainAcessorios">
-            <div class="title">
-                <h3>Acessórios</h3>
-            </div>
-            <div class="mainProdutos">
-                <div class="produtos">
-                    <img src="../assets/img/Acessorios/bag.jpg" alt="Bag" />
-                </div>
-                <div class="produtos">
-                    <img src="../assets/img/Acessorios/cap.webp" alt="Cap" />
-                </div>
-                <div class="produtos">
-                    <img src="../assets/img/Acessorios/bolsa.webp" alt="Bolsa" />
-                </div>
-                <div class="produtos">
-                    <img src="../assets/img/Acessorios/touca.webp" alt="Touca" />
-                </div>
-            </div>
+  <div class="acessorios">
+    <div class="mainAcessorios">
+      <div class="title">
+        <h3>Acessórios</h3>
+      </div>
+      <div class="mainProdutos">
+        <div class="produtos">
+          <img src="../assets/img/Acessorios/bag.jpg" alt="Bag" />
+          <div class="infoProdutos">
+            <button class="btn-more" @click="showMoreDetails">Saiba Mais</button>
+            <button class="btn-cart" @click="addToCart">
+              <img src="../assets/img/carrinho.svg" alt="Carrinho" class="cart-image" />
+            </button>
+          </div>
         </div>
-        <div class="imgAcessorios">
-            <img src="../assets/img/Acessorios/Acessorios.webp" alt="Acessórios">
+        <div class="produtos">
+          <img src="../assets/img/Acessorios/cap.webp" alt="Cap" />
+          <div class="infoProdutos">
+            <button class="btn-more" @click="showMoreDetails">Saiba Mais</button>
+            <button class="btn-cart" @click="addToCart">
+              <img src="../assets/img/carrinho.svg" alt="Carrinho" class="cart-image" />
+            </button>
+          </div>
         </div>
+        <div class="produtos">
+          <img src="../assets/img/Acessorios/bolsa.webp" alt="Bolsa" />
+          <div class="infoProdutos">
+            <button class="btn-more" @click="showMoreDetails">Saiba Mais</button>
+            <button class="btn-cart" @click="addToCart">
+              <img src="../assets/img/carrinho.svg" alt="Carrinho" class="cart-image" />
+            </button>
+          </div>
+        </div>
+        <div class="produtos">
+          <img src="../assets/img/Acessorios/touca.webp" alt="Touca" />
+          <div class="infoProdutos">
+            <button class="btn-more" @click="showMoreDetails">Saiba Mais</button>
+            <button class="btn-cart" @click="addToCart">
+              <img src="../assets/img/carrinho.svg" alt="Carrinho" class="cart-image" />
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
+    <div class="imgAcessorios">
+      <img src="../assets/img/Acessorios/Acessorios.webp" alt="Acessórios" />
+    </div>
+  </div>
 </template>
+<script>
+  export default {
+    props: {
+      product: {
+        type: Object,
+        required: true,
+      },
+    },
+    methods: {
+      showMoreDetails() {
+        alert(`Mais detalhes sobre: ${this.product.name}`);
+      },
+      addToCart() {
+        alert(`${this.product.name} foi adicionado ao carrinho!`);
+      },
+    },
+  };
+</script>
 
-<style>
+<style scoped>
 .acessorios {
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-wrap: wrap;
-    padding: 20px 0;
-    gap: 10px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  padding: 20px 0;
+  gap: 10px;
 }
 
 .acessorios .mainAcessorios {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    gap: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 30px;
 }
 
 .acessorios .mainAcessorios .title h3 {
-    font-size: 22px;
+  font-size: 22px;
 }
 
 .acessorios .mainAcessorios .mainProdutos {
-    width: 50%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 30px;
-    flex-wrap: wrap;
+  width: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+  flex-wrap: wrap;
 }
 
 .acessorios .mainAcessorios .mainProdutos .produtos {
-    width: 280px;
-    height: 280px;
-    border: 1px solid #e7e7e7;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  border: 1px solid #e7e7e7;
+  transition: all .5s ease;
+}
+
+.acessorios .mainAcessorios .mainProdutos .produtos:hover{
+    transform: scale(1.01);
+}
+
+.infoProdutos{
+    display: flex;
+    opacity: 0;
+    justify-content: space-between;
+    width: 100%;
+    transition: all .5s ease;
+    padding: 0 45px 20px 45px;
+}
+
+.acessorios .mainAcessorios .mainProdutos .produtos .infoProdutos .cart-image {
+    width: 36px;
+    height: 36px;
+  }
+
+  .acessorios .mainAcessorios .mainProdutos .produtos .infoProdutos button{
+    background: transparent;
+  }
+
+.acessorios .mainAcessorios .mainProdutos .produtos:hover .infoProdutos {
+  opacity: 1;  
 }
 
 .acessorios .mainAcessorios .mainProdutos .produtos img {
-    width: 278px;
-    height: 278px;
+  width: 278px;
+  height: 278px;
 }
 
 .acessorios .imgAcessorios {
-    border: 1px solid #e7e7e7;
-    width: 550px;
-    height: 572px;
-    transform: translateX(-120px);
-    margin-top: 20px;
+  border: 1px solid #e7e7e7;
+  width: 550px;
+  height: 572px;
+  transform: translateX(-120px);
+  margin-top: 20px;
 }
 
 .acessorios .imgAcessorios img {
-    width: 550px;
-    height: 570px;
+  width: 550px;
+  height: 570px;
+  border: 1px solid #e7e7e7;
 }
 </style>
--- src/components/RoupasComponente.vue
+++ src/components/RoupasComponente.vue
@@ -1,84 +1,155 @@
 <template>
-    <div class="roupas">
-        <div class="imgRoupas">
-            <img src="../assets/img/Roupas/Roupa.jpg" alt="Imagem de Roupas">
+    <div class="acessorios">
+      <div class="mainAcessorios">
+        <div class="title">
+          <h3>Roupas</h3>
         </div>
-        <div class="mainRoupas">
-            <div class="title">
-                <h3>Roupas</h3>
+        <div class="mainProdutos">
+          <div class="produtos">
+            <img src="../assets/img/roupas/calca.webp" alt="Bag" />
+            <div class="infoProdutos">
+              <button class="btn-more" @click="showMoreDetails">Saiba Mais</button>
+              <button class="btn-cart" @click="addToCart">
+                <img src="../assets/img/carrinho.svg" alt="Carrinho" class="cart-image" />
+              </button>
             </div>
-            <div class="mainProdutos">
-                <div class="produtos">
-                    <img src="../assets/img/Roupas/fuck.webp" alt="" />
-                </div>
-                <div class="produtos">
-                    <img src="../assets/img/Roupas/jacket.webp" alt="" />
-                </div>
-                <div class="produtos">
-                    <img src="../assets/img/Roupas/calca.webp" alt="" />
-                </div>
-                <div class="produtos">
-                    <img src="../assets/img/Roupas/moletom.webp" alt="" />
-                </div>
+          </div>
+          <div class="produtos">
+            <img src="../assets/img/roupas/fuck.webp" alt="Cap" />
+            <div class="infoProdutos">
+              <button class="btn-more" @click="showMoreDetails">Saiba Mais</button>
+              <button class="btn-cart" @click="addToCart">
+                <img src="../assets/img/carrinho.svg" alt="Carrinho" class="cart-image" />
+              </button>
             </div>
+          </div>
+          <div class="produtos">
+            <img src="../assets/img/roupas/jacket.webp" alt="Bolsa" />
+            <div class="infoProdutos">
+              <button class="btn-more" @click="showMoreDetails">Saiba Mais</button>
+              <button class="btn-cart" @click="addToCart">
+                <img src="../assets/img/carrinho.svg" alt="Carrinho" class="cart-image" />
+              </button>
+            </div>
+          </div>
+          <div class="produtos">
+            <img src="../assets/img/roupas/moletom.webp" alt="Touca" />
+            <div class="infoProdutos">
+              <button class="btn-more" @click="showMoreDetails">Saiba Mais</button>
+              <button class="btn-cart" @click="addToCart">
+                <img src="../assets/img/carrinho.svg" alt="Carrinho" class="cart-image" />
+              </button>
+            </div>
+          </div>
         </div>
+      </div>
+      <div class="imgAcessorios">
+        <img src="../assets/img/roupas/Roupa.jpg" alt="Acessórios" />
+      </div>
     </div>
-</template>
-
-<style>
-.roupas {
+  </template>
+  <script>
+    export default {
+      props: {
+        product: {
+          type: Object,
+          required: true,
+        },
+      },
+      methods: {
+        showMoreDetails() {
+          alert(`Mais detalhes sobre: ${this.product.name}`);
+        },
+        addToCart() {
+          alert(`${this.product.name} foi adicionado ao carrinho!`);
+        },
+      },
+    };
+  </script>
+  
+  <style scoped>
+  .acessorios {
     width: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
+    flex-direction: row-reverse;
     flex-wrap: wrap;
     padding: 20px 0;
-}
-
-.roupas .imgRoupas {
-    border: 1px solid #e7e7e7;
-    width: 552px;
-    height: 572px;
-    transform: translateX(310px);
-    /* Move 30px para a direita */
-}
-
-.roupas .imgRoupas img {
-    width: 550px;
-    height: 570px;
-}
-
-.roupas .mainRoupas {
+  }
+  
+  .acessorios .mainAcessorios {
     display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: column;
     gap: 30px;
-    transform: translateX(200px);
-    /* Move 20px para a esquerda */
-}
-
-.roupas .mainRoupas .title h3 {
+  }
+  
+  .acessorios .mainAcessorios .title h3 {
     font-size: 22px;
-}
-
-.roupas .mainRoupas .mainProdutos {
-    width: 50%;
+  }
+  
+  .acessorios .mainAcessorios .mainProdutos {
+    width: 45dvw;
     display: flex;
     justify-content: center;
     align-items: center;
     gap: 30px;
     flex-wrap: wrap;
-}
-
-.roupas .mainRoupas .mainProdutos .produtos {
-    width: 280px;
-    height: 280px;
+  }
+  
+  .acessorios .mainAcessorios .mainProdutos .produtos {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
     border: 1px solid #e7e7e7;
-}
-
-.roupas .mainRoupas .mainProdutos .produtos img {
+    transition: all .5s ease;
+  }
+  
+  .acessorios .mainAcessorios .mainProdutos .produtos:hover{
+      transform: scale(1.01);
+  }
+  
+  .infoProdutos{
+      display: flex;
+      opacity: 0;
+      justify-content: space-between;
+      width: 100%;
+      transition: all .5s ease;
+      padding: 0 45px 20px 45px;
+  }
+  
+  .acessorios .mainAcessorios .mainProdutos .produtos .infoProdutos .cart-image {
+      width: 36px;
+      height: 36px;
+    }
+  
+    .acessorios .mainAcessorios .mainProdutos .produtos .infoProdutos button{
+      background: transparent;
+    }
+  
+  .acessorios .mainAcessorios .mainProdutos .produtos:hover .infoProdutos {
+    opacity: 1;  
+  }
+  
+  .acessorios .mainAcessorios .mainProdutos .produtos img {
     width: 278px;
     height: 278px;
-}
-</style>
+  }
+  
+  .acessorios .imgAcessorios {
+    border: 1px solid #e7e7e7;
+    width: 550px;
+    height: 570px;
+    margin-top: 20px;
+  }
+  
+  .acessorios .imgAcessorios img {
+    width: 550px;
+    height: 570px;
+    border: 1px solid #e7e7e7;
+  }
+  </style>
+  
