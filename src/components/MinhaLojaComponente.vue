ce99ff377df3311315a427442c8ea2f5c0575b9b
DaviBisewski <davimbisewski@gmail.com>
2024-10-14 20:56:21 -0300

    FEAT: Pagina Minha loja adicionado produtos #22

--- src/components/MinhaLojaComponente.vue
+++ src/components/MinhaLojaComponente.vue
@@ -1,101 +1,133 @@
 <template>
     <div class="pagina-perfil">
-        <div class="banner">
-            <input type="file" id="banner-input" @change="atualizarBanner" accept="image/*" v-show="editar" />
-            <label v-show="editar" for="banner-input" class="botao-escolher-arquivo banner-button">+</label>
-            <img v-if="banner" :src="banner" alt="Banner" />
+      <div class="banner">
+        <input type="file" id="banner-input" @change="atualizarBanner" accept="image/*" v-show="editar" />
+        <label v-show="editar" for="banner-input" class="botao-escolher-arquivo banner-button">+</label>
+        <img v-if="banner" :src="banner" alt="Banner" />
+      </div>
+  
+      <div class="perfil-conteudo">
+        <div class="perfil-info">
+          <div class="imagem-bola">
+            <div class="bola">
+              <input type="file" id="imagem-bola-input" @change="atualizarImagemBola" accept="image/*" v-show="editar" />
+              <label v-show="editar" for="imagem-bola-input" class="botao-escolher-arquivo bola-button">+</label>
+              <img v-if="imagemBola" :src="imagemBola" alt="Imagem do Usuário" />
+            </div>
+          </div>
+          <div class="informacoes-usuario">
+            <h2>{{ nome }}</h2>
+            <p>{{ cidade }}, {{ estado }}</p>
+          </div>
         </div>
-
-        <div class="perfil-conteudo">
-            <div class="perfil-info">
-                <div class="imagem-bola">
-                    <div class="bola">
-                        <input type="file" id="imagem-bola-input" @change="atualizarImagemBola" accept="image/*"
-                            v-show="editar" />
-                        <label v-show="editar" for="imagem-bola-input"
-                            class="botao-escolher-arquivo bola-button">+</label>
-                        <img v-if="imagemBola" :src="imagemBola" alt="Imagem do Usuário" />
-                    </div>
-                </div>
-                <div class="informacoes-usuario">
-                    <h2>{{ nome }}</h2>
-                    <p>{{ cidade }}, {{ estado }}</p>
-                </div>
-            </div>
-            <div class="botoes-perfil">
-                <button @click="toggleEditar" class="botao-editar-perfil">
-                    {{ editar ? 'Salvar' : 'Editar Perfil' }}
-                </button>
-                <router-link to="/addproduto">
-                    <button class="botao-produto">+ Produto</button>
-                </router-link>
-            </div>
-
-            <div class="avaliacoes">
-                <div class="estrelas">
-                    <span v-for="star in 5" :key="star" class="estrela">&#9733;</span>
-                </div>
-                <p>({{ numeroAvaliacoes }})</p>
-            </div>
+        <div class="botoes-perfil">
+          <button @click="toggleEditar" class="botao-editar-perfil">
+            {{ editar ? 'Salvar' : 'Editar Perfil' }}
+          </button>
+          <router-link to="/addproduto">
+            <button class="botao-produto">+ Produto</button>
+          </router-link>
         </div>
-        <h1 class="tittle">Produtos</h1>
+  
+        <div class="avaliacoes">
+          <div class="estrelas">
+            <span v-for="star in 5" :key="star" class="estrela">&#9733;</span>
+          </div>
+          <p>({{ numeroAvaliacoes }})</p>
+        </div>
+      </div>
+      <h1 class="tittle">Produtos</h1>
+  
+      <div class="produtos">
+        <CardProdutoComponente
+          v-for="(produto, index) in produtos"
+          :key="index"
+          :produto="produto"
+        />
+      </div>
     </div>
-</template>
-
-<script>
-export default {
+  </template>
+  
+  <script>
+  import { ref } from 'vue'
+  import CardProdutoComponente from '../components/CardProdutoComponente.vue';
+  
+  export default {
     name: 'MinhaPagina',
-    data() {
-        return {
-            editar: false,
-            banner: null,
-            imagemBola: null,
-            nome: 'Nome do Usuário',
-            cidade: 'Cidade',
-            estado: 'Estado',
-            numeroAvaliacoes: 0,
-        };
+    components: {
+      CardProdutoComponente,
     },
-    methods: {
-        toggleEditar() {
-            this.editar = !this.editar;
-        },
-        atualizarBanner(event) {
-            const file = event.target.files[0];
-            if (file) {
-                this.banner = URL.createObjectURL(file);
-            }
-        },
-        atualizarImagemBola(event) {
-            const file = event.target.files[0];
-            if (file) {
-                this.imagemBola = URL.createObjectURL(file);
-            }
-        },
-    }
-}
-</script>
-
-<style scoped>
-.pagina-perfil {
+    setup() {
+      const editar = ref(false);
+      const banner = ref(null);
+      const imagemBola = ref(null);
+      const nome = ref('Nome do Usuário');
+      const cidade = ref('Cidade');
+      const estado = ref('Estado');
+      const numeroAvaliacoes = ref(0);
+      const produtos = Array.from({ length: 12 }, (_, index) => ({
+        id: index + 1,
+        nome: `Produto ${index + 1}`,
+        preco: Math.random() * 100, // Gerando preços aleatórios para os produtos
+        vendido: Math.random() > 0.5, // Definindo aleatoriamente se o produto foi vendido
+        imagem: 'https://droper.app/marca/corteiz?utm_source=google&utm_medium=paid_pmax&utm_campaign=%5BFIARA%5D%5BP.MAX%5D%5BVENDAS%5D%5BNC%5D%5BINSTITUCIONAL%5D&utm_content=&utm_id=Cj0KCQjwgrO4BhC2ARIsAKQ7zUkRxEcj5xuqR1i0WvYDNzbphI90YlcJKw0PzjQjDQ_j8nvKAqL5mlQaAvzDEALw_wcB&gad_source=1&gclid=Cj0KCQjwgrO4BhC2ARIsAKQ7zUkRxEcj5xuqR1i0WvYDNzbphI90YlcJKw0PzjQjDQ_j8nvKAqL5mlQaAvzDEALw_wcB'
+      }));
+  
+      const toggleEditar = () => {
+        editar.value = !editar.value;
+      };
+  
+      const atualizarBanner = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+          banner.value = URL.createObjectURL(file);
+        }
+      };
+  
+      const atualizarImagemBola = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+          imagemBola.value = URL.createObjectURL(file);
+        }
+      };
+  
+      return {
+        editar,
+        banner,
+        imagemBola,
+        nome,
+        cidade,
+        estado,
+        numeroAvaliacoes,
+        produtos,
+        toggleEditar,
+        atualizarBanner,
+        atualizarImagemBola,
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .pagina-perfil {
     padding: 20px;
     width: 80%;
     margin: 0 auto;
-}
-
-.banner {
+  }
+  
+  .banner {
     position: relative;
     width: 100%;
     height: 400px;
     background-color: #f0f0f0;
     margin-bottom: 20px;
-}
-
-.banner input {
+  }
+  
+  .banner input {
     display: none;
-}
-
-.banner-button {
+  }
+  
+  .banner-button {
     position: absolute;
     top: 50%;
     left: 50%;
@@ -108,26 +140,25 @@ export default {
     opacity: 0.5;
     transition: opacity 0.3s;
     z-index: 1;
-}
-
-.banner-button:hover {
+  }
+  
+  .banner-button:hover {
     opacity: 1;
-}
-
-.banner img {
+  }
+  
+  .banner img {
     width: 100%;
     height: 100%;
     object-fit: cover;
-}
-
-.botoes-perfil {
+  }
+  
+  .botoes-perfil {
     display: flex;
     gap: 10px;
-    /* Espaçamento entre os botões */
-}
-
-.botao-editar-perfil,
-.botao-produto {
+  }
+  
+  .botao-editar-perfil,
+  .botao-produto {
     cursor: pointer;
     background-color: #9c1e0e;
     border-radius: 10px;
@@ -135,31 +166,31 @@ export default {
     height: 50px;
     color: white;
     margin-top: 20px;
-}
-
-.botao-produto:hover {
+  }
+  
+  .botao-produto:hover {
     background-color: #5a140b;
-}
-
-.botao-editar-perfil:hover {
+  }
+  
+  .botao-editar-perfil:hover {
     background-color: #5a140b;
-}
-
-.perfil-conteudo {
+  }
+  
+  .perfil-conteudo {
     display: flex;
     justify-content: space-between;
-}
-
-.perfil-info {
+  }
+  
+  .perfil-info {
     display: flex;
-}
-
-.imagem-bola {
+  }
+  
+  .imagem-bola {
     position: relative;
     margin-right: 20px;
-}
-
-.bola {
+  }
+  
+  .bola {
     width: 100px;
     height: 100px;
     background-color: #d9d9d9;
@@ -168,20 +199,20 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
-}
-
-.bola input {
+  }
+  
+  .bola input {
     display: none;
-}
-
-.bola img {
+  }
+  
+  .bola img {
     width: 100%;
     height: 100%;
     border-radius: 50%;
     object-fit: cover;
-}
-
-.bola-button {
+  }
+  
+  .bola-button {
     position: absolute;
     font-size: 24px;
     color: #000;
@@ -190,38 +221,46 @@ export default {
     cursor: pointer;
     opacity: 0.5;
     transition: opacity 0.3s;
-}
-
-.bola-button:hover {
+  }
+  
+  .bola-button:hover {
     opacity: 1;
-}
-
-.informacoes-usuario {
+  }
+  
+  .informacoes-usuario {
     display: flex;
     flex-direction: column;
     justify-content: center;
-}
-
-.avaliacoes {
+  }
+  
+  .avaliacoes {
     display: flex;
     flex-direction: column;
     align-items: flex-end;
-}
-
-.estrelas {
+  }
+  
+  .estrelas {
     color: #d9d9d9;
-}
-
-.estrelas .estrela {
+  }
+  
+  .estrelas .estrela {
     font-size: 25px;
-}
-
-.tittle {
+  }
+  
+  .tittle {
     margin: auto;
     align-items: center;
     justify-content: center;
     display: flex;
     padding: 40px;
     border-bottom: 2px solid #9c1e0e;
-}
-</style>
+  }
+  
+  .produtos {
+    padding-top: 40px;
+    display: flex; /* Adicionado para exibir os cartões em linha */
+    flex-wrap: wrap; /* Permitir quebra de linha */
+    justify-content: center; /* Justifica à esquerda */
+  }
+  </style>
+  
--- src/views/AdidasView.vue
+++ src/views/AdidasView.vue
@@ -1,32 +1,28 @@
 <template>
-    <div class="page-container">
-   
-  
-      <div class="logo-container">
-        <img src="../assets/img/adidas.svg" alt="Adidas Logo" class="adidas-logo" />
-      </div>
-  
-      <div class="main-content">
-        <!-- Filtro -->
-        <aside class="filter-container">
-          <input type="text" placeholder="Filtrar produtos" class="filter-input" />
-        </aside>
-  
-        <!-- Lista de produtos -->
-        <div class="product-list">
-          <ProductItem
-            v-for="(product, index) in products"
-            :key="index"
-            :product="product"
-          />
-        </div>
+  <div class="page-container">
+
+
+    <div class="logo-container">
+      <img src="../assets/img/adidas.svg" alt="Adidas Logo" class="adidas-logo" />
+    </div>
+
+    <div class="main-content">
+      <!-- Filtro -->
+      <aside class="filter-container">
+        <input type="text" placeholder="Filtrar produtos" class="filter-input" />
+      </aside>
+
+      <!-- Lista de produtos -->
+      <div class="product-list">
+        <ProductItem v-for="(product, index) in products" :key="index" :product="product" />
       </div>
     </div>
-  </template>
-  
-  <script>
-  import ProductItem from '../components/ProductItem.vue';
-  import tenis1adidas from '../assets/produtos.adidas/tenis1adidas.webp'
+  </div>
+</template>
+
+<script>
+import ProductItem from '../components/ProductItem.vue';
+import tenis1adidas from '../assets/produtos.adidas/tenis1adidas.webp'
 import mochila2adidas from '../assets/produtos.adidas/mochila2adidas.jpg'
 import mochila1adidas from '../assets/produtos.adidas/mochila1adidas.webp'
 import camisa3adidas from '../assets/produtos.adidas/camisa3adidas.webp'
@@ -39,14 +35,14 @@ import tenis5adidas from '../assets/produtos.adidas/tenis5adidas.webp'
 import tenis6adidas from '../assets/produtos.adidas/tenis6adidas.webp'
 import bag1adidas from '../assets/produtos.adidas/bag1adidas.webp'
 
-  
-  export default {
-    components: {
-      ProductItem,
-    },
-    data() {
-      return {
-        products: [
+
+export default {
+  components: {
+    ProductItem,
+  },
+  data() {
+    return {
+      products: [
         { name: 'Produto 1', description: 'Descrição do produto 1', image: tenis1adidas },
         { name: 'Produto 2', description: 'Descrição do produto 2', image: mochila2adidas },
         { name: 'Produto 3', description: 'Descrição do produto 3', image: camisa3adidas },
@@ -54,68 +50,68 @@ import bag1adidas from '../assets/produtos.adidas/bag1adidas.webp'
         { name: 'Produto 5', description: 'Descrição do produto 6', image: camisa2adidas },
         { name: 'Produto 6', description: 'Descrição do produto 7', image: camisa1adidas },
         { name: 'Produto 7', description: 'Descrição do produto 8', image: tenis3adidas },
-        { name: 'Produto 8', description: 'Descrição do produto 11', image: tenis4adidas  },
-        { name: 'Produto 9', description: 'Descrição do produto 12', image: tenis5adidas  },
-        { name: 'Produto 10', description: 'Descrição do produto 13', image: mochila1adidas  },
-        { name: 'Produto 11', description: 'Descrição do produto 14', image: tenis6adidas  },
+        { name: 'Produto 8', description: 'Descrição do produto 11', image: tenis4adidas },
+        { name: 'Produto 9', description: 'Descrição do produto 12', image: tenis5adidas },
+        { name: 'Produto 10', description: 'Descrição do produto 13', image: mochila1adidas },
+        { name: 'Produto 11', description: 'Descrição do produto 14', image: tenis6adidas },
         { name: 'Produto 12', description: 'Descrição do produto 3', image: bag1adidas },
-        ],
-      };
-    },
-  };
-  </script>
-  
-  <style scoped>
-  .page-container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 20px;
-  }
-  
-  header {
-    width: 100%;
-  }
-  
-  .logo-container {
-    margin: 20px 0;
-  }
-  
-  .adidas-logo {
-    width: 150px;
-    height: auto;
-  }
-  
-  .main-content {
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    align-items: center;
-  }
-  
-  .filter-container {
-    width: 100%;
-    max-width: 600px;
-    padding: 10px;
-    box-sizing: border-box;
-  }
-  
-  .filter-input {
-    width: 100%;
-    padding: 10px;
-    font-size: 16px;
-    border: 1px solid #ccc;
-    margin-bottom: 20px; /* Espaço abaixo do campo de filtro */
-  }
-  
-  .product-list {
-display: flex;
-justify-content: center;
-align-items: center;
-flex-wrap: wrap;
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.page-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+}
+
+header {
+  width: 100%;
+}
+
+.logo-container {
+  margin: 20px 0;
+}
+
+.adidas-logo {
+  width: 150px;
+  height: auto;
+}
+
+.main-content {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: center;
+}
+
+.filter-container {
+  width: 100%;
+  max-width: 600px;
+  padding: 10px;
+  box-sizing: border-box;
+}
+
+.filter-input {
+  width: 100%;
+  padding: 10px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  margin-bottom: 20px;
+  /* Espaço abaixo do campo de filtro */
+}
+
+.product-list {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
   gap: 20px;
   padding: 20px;
   max-width: 1500px;
 }
-  </style>
-  
+</style>
