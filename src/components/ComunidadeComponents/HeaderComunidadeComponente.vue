b8d04f7a0b20665f48a9c76435b030619e8e3088
DaviBisewski <davimbisewski@gmail.com>
2024-10-30 22:37:19 -0300

    FEAT: Header Comunidade Responsivo #34

--- src/components/ComunidadeComponents/HeaderComunidadeComponente.vue
+++ src/components/ComunidadeComponents/HeaderComunidadeComponente.vue
@@ -1,325 +1,311 @@
 <template>
-    <header>
-        <div class="mainHeader">
-            <div class="logo-search">
-                <div class="logo">
-                    <RouterLink to="/"><img src="/src/assets/img/Logos/logo.png" alt="Logo"></RouterLink>
-                </div>
-                <div class="search-bar">
-                    <input type="text" placeholder="Pesquisar...">
-                    <button class="search-button"></button>
-                </div>
-            </div>
-
-            <nav class="nav">
-                <RouterLink to="/roupas" class="routerLink">
-                    <p>Roupas</p>
-                </RouterLink>
-                <RouterLink to="/sneakers" class="routerLink">
-                    <p>Calçados</p>
-                </RouterLink>
-                <RouterLink to="/acessorios" class="routerLink">
-                    <p>Acessórios</p>
-                </RouterLink>
-            </nav>
-
-            <div class="icon">
-                <button @click="showLogin = !showLogin" class="icon-button">
-                    <img src="/src/assets/img/Icons/user.svg" alt="Usuário">
-                </button>
-                <button @click="mostrarEsconder" class="icon-button">
-                    <img src="/src/assets/img/Icons/carrinho.svg" alt="Carrinho">
-                </button>
-                <router-link to="/minhaloja">
-                    <button class="quero-vender-button">Quero Vender</button>
-                </router-link>
-            </div>
+  <header>
+    <div class="mainHeader">
+      <div class="hamburger" @click="toggleMenu">
+        <div :class="{'open': isMenuOpen}">
+          <div class="bar"></div>
+          <div class="bar"></div>
+          <div class="bar"></div>
         </div>
+      </div>
 
-        <LoginComponente v-if="showLogin" />
-        <div class="divCarrinho" v-if="mostrar">
-            <div class="fecharCarrinho">
-                <button @click="mostrarEsconder">
-                    <img src="/src/assets/img/Icons/excluir.png" alt="">
-                </button>
-            </div>
-            <div class="titleCarrinho">
-                <h2>CARRINHO</h2>
-            </div>
-            <div class="produtosCarrinho">
-                <div class="closeProduto">
-                    <button>
-                        <img src="/src/assets/img/Icons/excluir (1).png" alt="">
-                    </button>
-                </div>
-                <div class="infoProduto">
-                    <div>
-                        <p>CAMISETA NIKE NRG MAX90 BT2</p>
-                        <p>R$174,99</p>
-                    </div>
-                    <div>
-                        <input type="number">
-                    </div>
-                </div>
-                <div class="imgProduto"><img src="/src/assets/produtos.nike/camisa3nike.webp" alt=""></div>
-            </div>
-            <div class="infoCarrinho">
-                <div class="boxFrete">
-                    <label>Calcular Frete:</label>
-                    <input type="text" value="00000-000">
-                </div>
-                <div class="boxTotalaPagar">
-                    <label>Total a Pagar: R$</label>
-                    <input type="text" value="174,99">
-                </div>
-            </div>
-            <div class="buttonComprarCarrinho">
-                <button>COMPRAR</button>
-            </div>
+      <button v-if="windowWidth <= 768 && !isSearchOpen" class="search-button" @click="toggleSearchBar">
+        <img src="/src/assets/img/Icons/search.png" alt="Pesquisar" />
+      </button>
+
+      <div class="logo" v-if="!isSearchOpen || windowWidth > 768">
+        <RouterLink to="/">
+          <img src="/src/assets/img/Logos/logo.png" alt="Logo" />
+        </RouterLink>
+      </div>
+
+      <div class="search-bar" :class="{'full-width': isSearchOpen || windowWidth > 768}">
+        <div v-if="isSearchOpen || windowWidth > 768" class="search-input-container">
+          <button v-if="windowWidth <= 768" @click="toggleSearchBar" class="close-search">X</button>
+          <input type="text" placeholder="Pesquisar..." :class="{'small-input': windowWidth > 768}" />
         </div>
-    </header>
+      </div>
+
+      <nav class="nav" :class="{'active': isMenuOpen}">
+        <button class="close-menu" v-if="isMenuOpen" @click="toggleMenu">X</button>
+        <RouterLink to="/roupas" class="routerLink" @click="toggleMenu">
+          <p>Roupas</p>
+        </RouterLink>
+        <RouterLink to="/sneakers" class="routerLink" @click="toggleMenu">
+          <p>Calçados</p>
+        </RouterLink>
+        <RouterLink to="/acessorios" class="routerLink" @click="toggleMenu">
+          <p>Acessórios</p>
+        </RouterLink>
+        <router-link to="/minhaloja" @click="toggleMenu">
+          <button class="quero-vender-button">Quero Vender</button>
+        </router-link>
+      </nav>
+
+      <div class="icon" v-if="!isSearchOpen || windowWidth > 768">
+        <button @click="showLogin = !showLogin" class="icon-button login-button">
+          <img src="/src/assets/img/Icons/user.svg" alt="Usuário" />
+        </button>
+        <button @click="mostrarEsconder" class="icon-button">
+          <img src="/src/assets/img/Icons/carrinho.svg" alt="Carrinho" />
+        </button>
+      </div>
+    </div>
+
+    <LoginComponente v-if="showLogin" />
+    <div class="divCarrinho" v-if="mostrar">
+      <div class="fecharCarrinho">
+        <button @click="mostrarEsconder">
+          <img src="/src/assets/img/Icons/excluir.png" alt="" />
+        </button>
+      </div>
+      <div class="titleCarrinho">
+        <h2>CARRINHO</h2>
+      </div>
+      <div class="produtosCarrinho">
+        <div class="closeProduto">
+          <button>
+            <img src="/src/assets/img/Icons/excluir (1).png" alt="" />
+          </button>
+        </div>
+        <div class="infoProduto">
+          <div>
+            <p>CAMISETA NIKE NRG MAX90 BT2</p>
+            <p>R$174,99</p>
+          </div>
+          <div>
+            <input type="number" />
+          </div>
+        </div>
+        <div class="imgProduto">
+          <img src="/src/assets/produtos.nike/camisa3nike.webp" alt="" />
+        </div>
+      </div>
+      <div class="infoCarrinho">
+        <div class="boxFrete">
+          <label>Calcular Frete:</label>
+          <input type="text" value="00000-000" />
+        </div>
+        <div class="boxTotalaPagar">
+          <label>Total a Pagar: R$</label>
+          <input type="text" value="174,99" />
+        </div>
+      </div>
+      <div class="buttonComprarCarrinho">
+        <button>COMPRAR</button>
+      </div>
+    </div>
+  </header>
 </template>
 
-<script>
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue';
 import LoginComponente from '@/components/BensaComponents/LoginComponente.vue';
 
+const showLogin = ref(false);
+const mostrar = ref(false);
+const isMenuOpen = ref(false);
+const isSearchOpen = ref(false);
+const windowWidth = ref(window.innerWidth);
 
-export default {
-    components: {
-        LoginComponente
-    },
-    data() {
-        return {
-            showLogin: false,
-            mostrar: false,
-        }
-    },
-    methods: {
-        mostrarEsconder() {
-            this.mostrar = !this.mostrar
-        }
-    }
-}
+const mostrarEsconder = () => {
+  mostrar.value = !mostrar.value;
+};
+
+const toggleMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value;
+};
+
+const toggleSearchBar = () => {
+  isSearchOpen.value = !isSearchOpen.value;
+};
+
+const updateWindowWidth = () => {
+  windowWidth.value = window.innerWidth;
+};
+
+onMounted(() => {
+  window.addEventListener('resize', updateWindowWidth);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', updateWindowWidth);
+});
 </script>
 
 <style scoped>
 .mainHeader {
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 10px 20px;
-    border-bottom: 1px solid #e0dbdb;
-    background-color: #ffffff;
-}
-
-.logo-search {
-    display: flex;
-    align-items: center;
-    margin-left: auto;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center; /* Centraliza os elementos */
+  padding: 10px 20px;
+  border-bottom: 1px solid #e0dbdb;
+  background-color: #ffffff;
 }
 
 .logo img {
-    width: 150px;
+  width: 150px;
+}
+
+.hamburger {
+  display: none;
+  cursor: pointer;
+}
+
+.hamburger .bar {
+  display: block;
+  width: 30px;
+  height: 3px;
+  margin: 6px 0;
+  background-color: #333;
+  transition: 0.4s;
 }
 
 .search-bar {
-    position: relative;
+  position: relative;
+  margin-left: 10px; /* Ajustado para espaço entre logo e input */
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
-.search-bar input {
-    padding: 14px 40px 8px 12px;
-    font-size: 16px;
-    border: 1px solid #ccc;
-    border-radius: 10px;
-    width: 70vh;
-    margin-left: 5px;
-    margin-right: 40px;
+.search-bar.full-width {
+  flex: 1;
+  justify-content: space-between;
+  padding: 0 10px;
 }
 
-.search-bar .search-button {
-    position: absolute;
-    right: 10px;
-    top: 50%;
-    transform: translateY(-50%);
-    background: none;
-    border: none;
-    cursor: pointer;
+.search-button {
+  background: none;
+  border: none;
+  cursor: pointer;
+}
+
+.search-button img {
+  width: 20px;
+}
+
+.search-input-container {
+  display: flex;
+  align-items: center;
+  width: 100%;
+}
+
+.search-input-container input {
+  padding: 10px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  width: 100%; /* Remover a largura fixa */
+  max-width: 300px; /* Definindo a largura máxima para telas pequenas */
+}
+
+.search-input-container .close-search {
+  background: none;
+  border: none;
+  cursor: pointer;
+  font-size: 18px;
+  margin-right: 10px;
+}
+
+/* Estilo para telas maiores */
+.search-input-container input.small-input {
+  width: 400px; /* Aumentar tamanho do input somente em telas grandes */
 }
 
 .nav {
-    display: flex;
-    align-items: center;
-    gap: 30px;
-}
-
-.nav .routerLink p {
-    color: #0d0d0d;
-    margin: 0;
-    cursor: pointer;
-    font-size: 18px;
+  display: flex;
+  align-items: center;
+  gap: 30px;
+  justify-content: center; /* Centraliza os links de navegação */
+  flex: 1; /* Para ocupar o espaço disponível */
 }
 
 .icon {
-    display: flex;
-    gap: 20px;
-    margin-left: auto;
-    margin-right: 70px;
+  display: flex;
+  gap: 20px;
 }
 
 .icon-button {
-    background: none;
-    border: none;
-    padding: 0;
-    cursor: pointer;
+  background: none;
+  border: none;
+  padding: 0;
+  cursor: pointer;
 }
 
 .icon-button img {
-    width: 30px;
-}
-
-.divCarrinho {
-    position: fixed;
-    right: 0;
-    top: 10px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    gap: 50px;
-    padding: 40px 60px;
-    background: #fff;
-    border-radius: 10px;
-    border: 1px solid #0d0d0d;
-    z-index: 1000000;
-}
-
-.divCarrinho .titleCarrinho {
-    font-size: 25px;
-}
-
-.divCarrinho .produtosCarrinho {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 15px;
-}
-
-.divCarrinho .produtosCarrinho .closeProduto button {
-    background: transparent;
-}
-
-.divCarrinho .produtosCarrinho .closeProduto img {
-    width: 25px;
-}
-
-.divCarrinho .produtosCarrinho .infoProduto {
-    display: flex;
-    gap: 20px;
-    background: #e7e7e7;
-    padding: 20px;
-}
-
-.divCarrinho .produtosCarrinho .infoProduto input {
-    width: 20px;
-}
-
-.divCarrinho .produtosCarrinho .imgProduto img {
-    width: 100px;
-}
-
-.divCarrinho .infoCarrinho {
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: start;
-    flex-direction: column;
-    gap: 7.3px;
-}
-
-.divCarrinho .infoCarrinho div {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 7.5px;
-}
-
-.divCarrinho .infoCarrinho .boxFrete {
-    font-size: 17px;
-}
-
-.divCarrinho .infoCarrinho .boxFrete input {
-    background: #e7e7e7;
-    padding: 5px 10px;
-    width: 95px;
-}
-
-.divCarrinho .infoCarrinho .boxTotalaPagar {
-    font-size: 13px;
-}
-
-.divCarrinho .buttonComprarCarrinho button {
-    padding: 10px 20px;
-    background: #e7e7e7;
-    border-radius: 12px;
-    font-size: 19px;
-    font-weight: 600;
-    transition: all .5s;
-}
-
-.divCarrinho .buttonComprarCarrinho:hover button {
-    transform: scale(1.1);
-}
-
-.fecharCarrinho {
-    position: absolute;
-    top: 40px;
-    right: 40px;
-}
-
-.fecharCarrinho button {
-    background: transparent;
-}
-
-.fecharCarrinho img {
-    width: 40px;
+  width: 35px;
 }
 
 .quero-vender-button {
-    background-color: #9c1e0e;
-    color: #fff;
-    padding: 10px 15px;
-    border: none;
-    border-radius: 10px;
-    cursor: pointer;
-    font-size: 16px;
+  background-color: #9c1e0e;
+  color: #fff;
+  padding: 10px 15px;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+  font-size: 16px;
 }
 
 .quero-vender-button:hover {
-    background-color: #69160b;
+  background-color: #69160b;
 }
 
-@media (max-width: 1100px) {
-    .mainHeader {
-        flex-direction: column;
-        align-items: flex-start;
-    }
+@media (max-width: 768px) {
+  .mainHeader {
+    justify-content: space-between; /* Mudar para espaçamento entre elementos */
+  }
 
-    .logo-search,
-    .nav,
-    .icon {
-        width: 100%;
-        justify-content: center;
-        margin: 10px 0;
-    }
+  .search-bar {
+    width: auto;
+    margin-top: 20px;
+  }
 
-    .nav {
-        flex-wrap: wrap;
-        gap: 15px;
-    }
+  .hamburger {
+    display: block;
+  }
 
-    .icon {
-        justify-content: flex-end;
-    }
+  .logo {
+    margin-left: 15px;
+  }
+
+  .logo img {
+    width: 110px;
+  }
+
+  .search-button {
+    margin-left: 25px; /* Espaço entre o botão de pesquisa e o menu hamburguer */
+  }
+
+  .search-button img {
+    width: 25px;
+  }
+
+  .nav {
+    position: absolute;
+    top: 60px;
+    left: 0;
+    right: 0;
+    background: #fff;
+    display: none;
+    flex-direction: column;
+    z-index: 10;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+  }
+
+  .nav.active {
+    display: flex;
+  }
+
+  .close-menu {
+    background: none;
+    border: none;
+    font-size: 24px;
+    cursor: pointer;
+    align-self: flex-end;
+  }
 }
 </style>
