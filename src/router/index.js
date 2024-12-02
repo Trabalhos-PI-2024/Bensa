f7210da25a648a75c7f4f4851deffb6a2d978408
DaviBisewski <davimbisewski@gmail.com>
2024-12-02 16:07:26 -0300

    FEAT: Ultimo commit eu espero pelo menos. #70

--- src/router/index.js
+++ src/router/index.js
@@ -143,8 +143,11 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes
-})
+  history: createWebHistory(''), 
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    return { top: 0 };
+  },
+});
 
 export default router
